7dd4597c061710c1c6be8c9e5775166da7774b41
Sérgio Camargo <sergiocamargo81@gmail.com>
2025-12-20 17:04:34 -0300

    2025.12.20 - Autofire Implementation

---
+++ keyboards/keychron/k10_max/ansi/rgb/keymaps/strauz/rules.mk
@@ -0,0 +1,11 @@
+# Seu keymap strauz - Firmware Puro (sem VIA)
+RGB_MATRIX_ENABLE = yes
+CONSOLE_ENABLE = no
+VIA_ENABLE = yes
+
+# Otimizações
+LTO_ENABLE = yes
+
+# Features extras (opcional - descomente se quiser)
+# EXTRAKEY_ENABLE = yes
+# MOUSEKEY_ENABLE = no
