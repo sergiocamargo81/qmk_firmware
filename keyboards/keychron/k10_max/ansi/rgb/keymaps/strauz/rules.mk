e870477e35f77e841ea6e0bb3b2699c645b59119
Sérgio Camargo <sergiocamargo81@gmail.com>
2025-12-21 17:02:07 -0300

    2025.12.21 - Autofire performance boost

--- keyboards/keychron/k10_max/ansi/rgb/keymaps/strauz/rules.mk
+++ keyboards/keychron/k10_max/ansi/rgb/keymaps/strauz/rules.mk
@@ -1,12 +1,7 @@
-# Seu keymap strauz - Firmware Puro (sem VIA)
 RGB_MATRIX_ENABLE = yes
 CONSOLE_ENABLE = no
 VIA_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
-
-# Otimizações
 LTO_ENABLE = yes
-
-# Features extras (opcional - descomente se quiser)
 # EXTRAKEY_ENABLE = yes
 # MOUSEKEY_ENABLE = no
